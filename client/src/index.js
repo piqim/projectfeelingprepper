4d6b5758fc37fda679e116a00b5e9be1007552e0
Piqim <115794663+piqim@users.noreply.github.com>
2025-09-11 17:36:37 -0700

    Created, and finished routes.

--- client/src/index.js
+++ client/src/index.js
@@ -1,68 +1,86 @@
 /* BASE REACT COMPONENT IMPORT */
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 /* CSS IMPORT */
-import './index.css';
+import "./index.css";
 
 /* PAGE COMPONENTS IMPORT */
-import App from './App';
+import App from "./App";
 // BASE COMPONENTS
-import Home from './components/Home.js';
-import Login from './components/Login.js';
-import Register from './components/Register.js';
-import Navbar from "./components/Navbar.js";
-import Footer from "./components/Footer.js";
-import reportWebVitals from './reportWebVitals';
+import Home from "./components/Home.js";
+import Login from "./components/Login.js";
+import Register from "./components/Register.js";
+import reportWebVitals from "./reportWebVitals";
 // PAGES COMPONENTS
-import Cogtri from './components/pages/Cogtri.js';
-import Grapes from './components/pages/Grapes.js';
-import Learnmore from './components/pages/Learnmore.js'
+import Cogtri from "./components/pages/Cogtri.js";
+import Grapes from "./components/pages/Grapes.js";
+import Learnmore from "./components/pages/Learnmore.js";
 import Settings from "./components/pages/Settings.js";
 import Tipp from "./components/pages/Tipp.js";
 import Dev from "./components/pages/Dev.js";
 
 /* ROUTE(S) */
 const router = createBrowserRouter([
-  
   /* CLIENT SIDE ROUTE(S) */
   {
     path: "/",
-    element: 
-    <App />,
+    element: <App />,
     children: [
       { index: true, element: <Home /> },
+
+      // MAIN PAGES
       {
-        path: "scholar",
-        children: [
-          { index: true, element: <Scholar /> },
-          { path: "detail/:id", element: <ScholarDetail /> },
-        ],
+        path: "cogtri",
+        children: [{ index: true, element: <Cogtri /> }],
       },
       {
-        path: "scholarship",
-        children: [
-          { index: true, element: <Scholarship /> },
-          { path: "detail/:id", element: <ScholarshipDetail /> },
-        ],
+        path: "grapes",
+        children: [{ index: true, element: <Grapes /> }],
+      },
+      {
+        path: "tipp",
+        children: [{ index: true, element: <Tipp /> }],
+      },
+      {
+        path: "learnmore",
+        children: [{ index: true, element: <Learnmore /> }],
       },
     ],
   },
 
+  /* USER ROUTE */
+  {
+    path: "/user",
+    element: <App />,
+    children: [
+      {
+        path: "login",
+        children: [{ index: true, element: <Login /> }],
+      },
+      {
+        path: "register",
+        children: [{ index: true, element: <Register /> }],
+      },
+    ],
+  },
+
+  /* SETTINGS ROUTE */
+  {
+    path: "/settings",
+    element: <App/>,
+    children: [{ index: true, element: <Settings /> }],
+  },
 
   /* DEV ROUTE */
   {
     path: "/dev",
     element: <Dev />,
   },
-
 ]);
 
-
+/* PUBLISH ROUTES */
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
